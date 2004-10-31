4ad4f441f265abdb1067f412d93deb5d33e79898
Andy Bakun <github@thwartedefforts.org>
2004-10-31 23:14:05 +0000

    properly fill background optionally show seconds

--- Themes/Original/Clock.pm
+++ Themes/Original/Clock.pm
@@ -19,6 +19,8 @@ use Widget::Surface;
 
 use base 'Widget::Surface';
 
+my $show_seconds = 0;
+
 sub widget_initialize {
     my $this = shift;
     my %o = @_;
@@ -31,7 +33,8 @@ sub widget_initialize {
     $this->{bgcolor} = new SDL::Color(-r=>170, -g=>170, -b=>170);
     $this->{fgcolor} = new SDL::Color(-r=>0, -g=>0, -b=>0);
 
-    my $fontsize = 49;
+    my $fontsize;
+    $fontsize = $show_seconds ? 40 : 49;
     $this->{font} = new SDL::TTFont(-name=>"./fonts/Vera.ttf", -size=>$fontsize, -bg=>$this->{bgcolor}, -fg=>$this->{fgcolor});
     $this->{surface} = new SDL::Surface(-width=>$area->width(), -height=>$area->height(), -depth=>32);
     $this->surface($this->{surface});
@@ -47,16 +50,23 @@ sub update {
     my $ticks = $o{ticks};
     my $force = $o{force};
 
-    $this->update_every(30000);
+    $this->update_every($show_seconds ? 1000 : 30000);
     my($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time());
     my @lines = ();
-    push(@lines, sprintf('%d:%02d %s', ($hour < 12 ? ($hour ? $hour : 12 ) : $hour - 12), $min, ($hour < 12 ? 'am' : 'pm')));
+    my $msg;
+    if ($show_seconds) {
+        $msg = sprintf('%d:%02d:%02d %s', ($hour < 12 ? ($hour ? $hour : 12 ) : $hour - 12), $min, $sec, ($hour < 12 ? 'am' : 'pm'));
+    } else {
+        $msg = sprintf('%d:%02d %s', ($hour < 12 ? ($hour ? $hour : 12 ) : $hour - 12), $min, ($hour < 12 ? 'am' : 'pm'));
+    }
+    push(@lines, $msg);
 
     my $x = freeze(\@lines);
     if ($force || $x ne $this->{lastlines}) {
         my $area = $this->area();
-        $this->container()->draw_background(canvas=>$this->{surface}, dest=>0, source=>$this->area());
-        $this->{font}->print_lines_justified(just=>-1, surf=>$this->{surface}, x=>0, y=>0, lines=>\@lines, maxwidth=>$area->width()-10);
+        my $surf = $this->surface();
+        $this->container()->draw_background(canvas=>$surf, dest=>0, source=>$area);
+        $this->{font}->print_lines_justified(just=>-1, surf=>$surf, x=>0, y=>0, lines=>\@lines, maxwidth=>$area->width()-10);
         $this->{lastlines} = $x;
         return 1;
     }
